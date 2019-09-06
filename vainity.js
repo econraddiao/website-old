abda020639f3aecec8e230269b8b217a04ab763d
econraddiao <ecdiao@umich.edu>
2019-09-06 16:30:52 -0700

    webkit

--- vainity.js
+++ vainity.js
@@ -3,6 +3,7 @@ var colorShifter = setInterval(shift, 500);
 function shift() {
     let aboutImg = document.getElementById("0").firstChild.firstChild;
     aboutImg.style.clipPath = "circle(50%)";
+    aboutImg.style.-webkit-clipPath = "circle(50%)";
     let color = "rgba(" + (Math.random()*255).toString() + "," + (Math.random()*255).toString() + "," + (Math.random()*255).toString() + ",1.00)"
     aboutImg.style.background = color;
 }
