2557693af454e44e12bbec7336bc1c11a2c4e921
econraddiao <ecdiao@umich.edu>
2019-09-14 14:43:33 -0700

    added some projects

--- projects.js
+++ projects.js
@@ -124,8 +124,6 @@ function clearProjects() {
 }
 
 function showAbout() {
-    clearProjects();
-    filterProjects("about");
     document.getElementById("projects-tab").classList.remove("active");
     document.getElementById("about-tab").classList.add("active");
 }
@@ -148,6 +146,8 @@ function launch(id) {
     let soloProject = document.getElementsByClassName("solo")[0];    
     let soloAnim = document.createElement("div");
     soloAnim.setAttribute("class", "launchAnim");
+    soloAnim.innerHTML = "BLASTOFF!";
+    soloProject.appendChild(soloAnim);
     soloAnim.style.position = "absolute";
     soloAnim.style.width = null;
     soloAnim.style.height = null;
@@ -155,37 +155,50 @@ function launch(id) {
     soloAnim.style.transition = "opacity .5s";
     soloAnim.style.background = "rgba(234, 64, 237, 1.00)";
     soloAnim.style.color = "rgba(115, 251, 253, 1.00)";    
-    soloAnim.innerHTML = "BLASTOFF!";
-    soloProject.appendChild(soloAnim);
 }
 
-let projects = [{
-        name: "Paper Cuts",
+let projects = [
+    {
+        name: "Caricature Cubes",
+        date: "Fall 2019",
+        description: "The contemporary virtual landscape is becoming more populated and polluted every day as our digital footprints are becoming unmanageably large, and our appearance and actions online are scrutinized by more and more friends or followers or fans. Our reaction to these trends is made clear by the rapid proliferation and adoption of automated assistants designed to simplify everything: finance, diet, shopping, scheduling, communicating, Et al. We are surrendering authorship and autonomy in as many areas of our lives as possible -- both virtual and real. Our stuff, the things that we consume and surround ourselves with, one of the most authentic and indeleble markers of our personalities. Caricature Cubes is a critical project which aims to highlight the absurdity of our impending virtualization of the real.  Consumerist and vain desires are understood as merely popular idiosyncrasies and by celebrating absurd conditions, we question our aesthetic and social celebrations and frustrate the images of our personalities.",
+        images: ["UG3_X4_1.jpg", "UG3_X4_2.jpg", "UG3_X4_3.jpg"],
+        tag: "architecture",
+    },
+    {
+        name: "West Park House",
+        date: "Winter 2018",
+        description: "The contemporary virtual landscape is becoming more populated and polluted every day as our digital footprints are becoming unmanageably large, and our appearance and actions online are scrutinized by more and more friends or followers or fans. Our reaction to these trends is made clear by the rapid proliferation and adoption of automated assistants designed to simplify everything: finance, diet, shopping, scheduling, communicating, Et al. We are surrendering authorship and autonomy in as many areas of our lives as possible -- both virtual and real. Our stuff, the things that we consume and surround ourselves with, one of the most authentic and indeleble markers of our personalities. Caricature Cubes is a critical project which aims to highlight the absurdity of our impending virtualization of the real.  Consumerist and vain desires are understood as merely popular idiosyncrasies and by celebrating absurd conditions, we question our aesthetic and social celebrations and frustrate the images of our personalities.",
+        images: ["UG2_X3_1.jpg", "UG2_X3_2.jpg"],
+        tag: "architecture",
+    },
+    {
+        name: "Plinth Platter House",
+        date: "Winter 2018",
+        description: "The contemporary virtual landscape is becoming more populated and polluted every day as our digital footprints are becoming unmanageably large, and our appearance and actions online are scrutinized by more and more friends or followers or fans. Our reaction to these trends is made clear by the rapid proliferation and adoption of automated assistants designed to simplify everything: finance, diet, shopping, scheduling, communicating, Et al. We are surrendering authorship and autonomy in as many areas of our lives as possible -- both virtual and real. Our stuff, the things that we consume and surround ourselves with, one of the most authentic and indeleble markers of our personalities. Caricature Cubes is a critical project which aims to highlight the absurdity of our impending virtualization of the real.  Consumerist and vain desires are understood as merely popular idiosyncrasies and by celebrating absurd conditions, we question our aesthetic and social celebrations and frustrate the images of our personalities.",
+        images: ["UG2_X2_1.jpg", "UG2_X2_2.jpg"],
+        tag: "architecture",
+    },
+    {
+        name: "Reality, Superimposed",
         date: "Fall 2016",
         description: "Paper Cuts is an study of the application of algorithmic subtractive manufacture of a keepsake. The book is a deeply cultral artifact, filled with humanity's stories and which serves as a phisical memory through additive manufacturing, specifically printing. This book offers a new paradigm of communication represents oncoming technological change. The book was designed parametrically by sampling random noise to generate seeds which served as the centroid of each cell which radiates to form a voronoi pattern. This pattern was copied in negative and positive along the head and tail of the book, adding to its complexity in fabrication.",
         images: ["202_X1_1.jpg", "202_X1_2.jpg", "202_X1_3.jpg", "202_X1_4.jpg"],
         tag: "architecture",
     },
     {
-        name: "American Classics",
-        date: "Fall 2017",
-        description: "Blah Blah",
-        images: ["_DSC3506.jpg", "_DSC2950.jpg"],
-        tag: "photography",
+        name: "Paper Cuts",
+        date: "Fall 2016",
+        description: "Paper Cuts is an study of the application of algorithmic subtractive manufacture of a keepsake. The book is a deeply cultral artifact, filled with humanity's stories and which serves as a phisical memory through additive manufacturing, specifically printing. This book offers a new paradigm of communication represents oncoming technological change. The book was designed parametrically by sampling random noise to generate seeds which served as the centroid of each cell which radiates to form a voronoi pattern. This pattern was copied in negative and positive along the head and tail of the book, adding to its complexity in fabrication.",
+        images: ["202_X3_1.jpg", "202_X3_2.jpg", "202_X3_3.jpg", "202_X3_4.jpg"],
+        tag: "architecture",
     },
     {
-        name: "Non-American Non-Classics ",
+        name: "Villa Sul Lago Study",
         date: "Fall 2018",
         description: "Bluh Blih",
-        images: ["_DSC2950.jpg", "_DSC3506.jpg"],
+        images: ["202_X4_1.jpg", "202_X4_2.jpg", "202_X4_3.jpg", "202_X4_4.jpg", "202_X4_5.jpg", "202_X4_6.jpg", "202_X4_7.jpg"],
         tag: "photography",
-    },
-    {
-        name: "Caricature Cubes",
-        date: "Fall 2019",
-        description: "The contemporary virtual landscape is becoming more populated and polluted every day as our digital footprints are becoming unmanageably large, and our appearance and actions online are scrutinized by more and more friends or followers or fans. Our reaction to these trends is made clear by the rapid proliferation and adoption of automated assistants designed to simplify everything: finance, diet, shopping, scheduling, communicating, Et al. We are surrendering authorship and autonomy in as many areas of our lives as possible -- both virtual and real. Our stuff, the things that we consume and surround ourselves with, one of the most authentic and indeleble markers of our personalities. Caricature Cubes is a critical project which aims to highlight the absurdity of our impending virtualization of the real.  Consumerist and vain desires are understood as merely popular idiosyncrasies and by celebrating absurd conditions, we question our aesthetic and social celebrations and frustrate the images of our personalities.",
-        images: ["UG3_X4_1.jpg"],
-        tag: "architecture",
     },
     {
         name: "hi.",
