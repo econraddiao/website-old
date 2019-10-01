cba654b6a6c8f86095a3baaeb880e68bfe45a15d
econraddiao <ecdiao@umich.edu>
2019-10-01 15:02:03 -0700

    closer

--- vainity.js
+++ vainity.js
@@ -1,8 +1,9 @@
 // JavaScript Document
+let item1 = document.getElementsByClassName("about")[0].firstChild;
+let aboutImg = item1.firstChild;
+
 let colorShifter = setInterval(shift, 500);
 function shift() {
-    let aboutImg = document.getElementsByClassName("about")[0].firstChild.firstChild;
-    aboutImg.style.cssText = "clip-path: circle(50%); -webkit-clip-path: circle(50%); max-width: 75%; margin: 50px";
     let color = "rgba(" + (Math.random()*255).toString() + "," + (Math.random()*255).toString() + "," + (Math.random()*255).toString() + ",1.00)"
     aboutImg.style.background = color;
 }
