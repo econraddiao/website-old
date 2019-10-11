99a233e3f6cb585807bfb2a1fa73ccd1f9592b57
econraddiao <ecdiao@umich.edu>
2019-10-11 15:15:34 -0700

    match height tidy

--- matchHeight.js
+++ matchHeight.js
@@ -5,9 +5,7 @@ window.addEventListener("resize", function () {
 
 function resizeAll() {
     let galleryElements = document.getElementsByClassName("project-container");
-    console.log(galleryElements);
         for(let els of galleryElements) {
-        console.log(els);
         let item1 = els.firstChild;
         let item2 = els.lastChild;
         let img = item1.firstChild;
