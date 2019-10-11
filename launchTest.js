d0168201362237d1989ebef467333698b33a388a
econraddiao <ecdiao@umich.edu>
2019-10-11 13:22:38 -0700

    >

--- launchTest.js
+++ launchTest.js
@@ -1,9 +1,1 @@
 // JavaScript Document
-
-var button = document.querySelector('.project-button');
-history.pushState(null, null, id)
-button.addEventListener('click', function() {
-    
-})
-projectsRequest.send();
-}
