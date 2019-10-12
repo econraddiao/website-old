32ac0d02470682f097c754f55ace5deffcf14531
econraddiao <ecdiao@umich.edu>
2019-10-11 17:31:43 -0700

    event bubbling

--- launch.js
+++ launch.js
@@ -1,30 +1,12 @@
 
 projectButton = document.querySelector("project-button");
 
-if (document.addEventListener) {
-    document.addEventListener("click", handleClick, false);
-}
-else if (document.attachEvent) {
-    document.attachEvent("onclick", handleClick);
-}
-
-function handleClick(event) {
-    event = event || window.event;
-    event.target = event.target || event.srcElement;
-
-    var element = event.target;
-
-    // Climb up the document tree from the target of the event
-    while (element) {
-        if (element.nodeName === "BUTTON") {
-            // The user clicked on a <button> or clicked on an element inside a <button>
-            // with a class name called "foo"
-            launch(element.parentNode.parentNode.id);
-            break;
-        }
-        element = element.parentNode;
+document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('project-button')) {
+        launch(event.target.parentNode.parentNode.id);
     }
-}
+}, false);
+
 
 function launch(id) {
     console.log("launching project " + id);
